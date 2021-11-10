544db59a10ebfc9ab765b9d2ebbb0dc8ae723a94
Nineves <ywang109@ntu.edu.sg>
2021-11-10 15:22:37 +0800

    inputHelper

---
+++ src/IntegerInputHelper.java
@@ -0,0 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class IntegerInputHelper {
+    public static int validateInput(int start, int end){
+        Scanner sc= new Scanner(System.in);
+        boolean valid = false;
+        int result=-1;
+        while (!valid){
+            try{
+                result=sc.nextInt();
+
+                if(result>=start&&result<=end){
+                    valid=true;
+                }
+                else {
+                    System.out.println("Input integer is not in the given range.");
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.println("Input is not an integer. Please enter again: ");
+            }
+            sc.nextLine(); //clears the buffer
+        }
+        return result;
+    }
+}
